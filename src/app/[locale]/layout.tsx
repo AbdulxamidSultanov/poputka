eb7d0b081886ae8b4ee8b6389ba451aceb4624b6
AbdulxamidSultanov <sultanovabdulxamid@gmail.com>
2025-12-08 06:14:37 +0500

    Theme switcher created, linted and builded

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,11 @@
 // src/app/[locale]/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { notFound } from "next/navigation";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import routing, { isValidLocale } from "@/i18n/routing";
+import {ThemeProvider} from "next-themes";
 import "./globals.scss";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "PopUtka Next",
@@ -44,11 +34,13 @@ export default async function LocaleLayout({
     const messages = await getMessages({ locale });
 
     return (
-        <html lang={locale}>
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <html lang={locale} suppressHydrationWarning>
+        <body>
+        <ThemeProvider disableTransitionOnChange={false} storageKey="theme" attribute="class" defaultTheme="system" enableSystem>
         <NextIntlClientProvider locale={locale} messages={messages}>
             {children}
         </NextIntlClientProvider>
+        </ThemeProvider>
         </body>
         </html>
     );
